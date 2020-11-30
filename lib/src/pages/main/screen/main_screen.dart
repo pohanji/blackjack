50b3afead56e65936fa6150e9a66727853fb2fbf
Jiří Pohančeník <pohanji17@sps-prosek.cz>
2020-11-30 11:09:33 +0100

    textures implementation

--- lib/src/pages/main/screen/main_screen.dart
+++ lib/src/pages/main/screen/main_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:blackjack/src/pages/rules/rules_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -5,20 +8,84 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Align(child: Text("BLACKJACK", style: TextStyle(fontSize: 35, color: Colors.white)),),
-          SizedBox(height: 30,),
-          OutlineButton(child: Text("ZAČÍT HRÁT", style: TextStyle(fontSize: 20)), color: Colors.white),
-          OutlineButton(child: Text("PRAVIDLA", style: TextStyle(fontSize: 20)), padding: const EdgeInsets.only(left: 25, right: 25),),
-          OutlineButton(child: Text("UKONČIT", style: TextStyle(fontSize: 20)), padding: const EdgeInsets.only(left: 30, right: 30)),
-        ],
-      )
-    );
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/backgrounds/pozadi.png"),
+                    fit: BoxFit.cover)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Align(
+                  child: Text("BLACKJACK",
+                      style: TextStyle(
+                          fontSize: 55,
+                          color: Colors.white,
+                          fontFamily: "Poker")),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                FlatButton(
+                  child: Text("ZAČÍT HRÁT",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontFamily: "Constantia",
+                      )),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    side: BorderSide(
+                      color: Colors.black87,
+                    ),
+                  ),
+                  color: Colors.white60,
+                  onPressed: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => null),
+                    )
+                  },
+                ),
+                FlatButton(
+                  child: Text("PRAVIDLA",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontFamily: "Constantia",
+                      )),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    side: BorderSide(
+                      color: Colors.black87,
+                    ),
+                  ),
+                  color: Colors.white60,
+                  padding: const EdgeInsets.only(left: 28, right: 28),
+                  onPressed: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => RulesScreen()),
+                    )
+                  },
+                ),
+                FlatButton(
+                  child: Text("UKONČIT",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontFamily: "Constantia",
+                      )),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    side: BorderSide(
+                      color: Colors.black87,
+                    ),
+                  ),
+                  padding: const EdgeInsets.only(left: 32, right: 32),
+                  color: Colors.white60,
+                  onPressed: () => exit(0),
+                ),
+              ],
+            )));
   }
-
 }
 
 class MainScreenState extends StatefulWidget {
@@ -26,5 +93,4 @@ class MainScreenState extends StatefulWidget {
   State<StatefulWidget> createState() {
     throw UnimplementedError();
   }
-
 }
