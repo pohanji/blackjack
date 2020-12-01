9324eee57a706ccf62f514fb915b0339cdca1500
Dominik Marsalek <domino01@gmail.com>
2020-12-01 16:03:37 +0100

    rules

--- lib/src/pages/rules/rules_screen.dart
+++ lib/src/pages/rules/rules_screen.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class RulesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Tady budou pravidla", style: TextStyle(fontSize: 45),),),);
+    return Scaffold(
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/backgrounds/pozadi.png"),
+                    fit: BoxFit.cover)),
+            child: Stack(
+              children: [
+                Align(
+                  child: Text("Pravidla",
+                  style: TextStyle(
+                  fontSize: 55,
+                  color: Colors.white,
+                  fontFamily: "Poker")),
+                  alignment: Alignment.topCenter,
+                ),
+
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Karty a jejich hodnoty:",
+                      style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: "Calibri",
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),),
+
+                    Text("Karty s hodnotou 2 - 10 mají hodnotu uvedenou na kartě.\nKarty J, Q a K mají hodnotu 10.\nKarta A může mít hodnotu 1 i 11. ",
+                      style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Calibri",
+                      color: Colors.white,
+                    ),),
+
+                    Text("Cíl hry:",
+                      style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: "Calibri",
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),),
+
+                    Text("Mít více než krupér.\nPokud hráč i krupiér překročí 21, vyhrává krupiér.\nV případě shodného počtu, krupiéra a hráče nevyhrává nikdo.",
+                      style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Calibri",
+                      color: Colors.white,
+                    ),),
+                  ],
+                )
+                
+              ],
+            
+            )));
   }
 
 }
